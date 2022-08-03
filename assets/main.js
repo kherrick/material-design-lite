1f6c461880471ff955177eca1db9529e1ef324ab
rodydavis <rodydavis@users.noreply.github.com>
2022-08-03 00:13:18 +0000

    deploy: f3fe6ffa4564ce09064824f8849e4a0d277d1d95

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,77 @@
-!function(){"use strict";function e(){c.classList.remove("disabled"),n.classList.remove("disabled"),l.scrollLeft<=0&&c.classList.add("disabled"),l.scrollLeft+l.clientWidth+5>=l.scrollWidth&&n.classList.add("disabled")}function t(e){l.scrollLeft+=e}var n=document.querySelector(".scrollindicator.scrollindicator--right"),c=document.querySelector(".scrollindicator.scrollindicator--left"),l=document.querySelector(".docs-navigation");l.addEventListener("scroll",e),e(),n.addEventListener("click",t.bind(null,40)),n.addEventListener("tap",t.bind(null,40)),c.addEventListener("click",t.bind(null,-40)),c.addEventListener("tap",t.bind(null,-40))}(),function(){"use strict";var e=document.querySelector(".download-button-container");e&&e.addEventListener("click",function(){ga("send",{hitType:"event",eventCategory:"click",eventAction:"mdl-download"})})}(),function(){"use strict";var e=document.querySelectorAll('[href=""]');Array.prototype.forEach.call(e,function(e){e.addEventListener("click",function(e){e.preventDefault()})})}();
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global MaterialCustomizer:true,Prism:true,ga:true*/
+
+// Navbar scroll buttons
+(function() {
+  'use strict';
+
+  var rightScroll = document.querySelector('.scrollindicator.scrollindicator--right');
+  var leftScroll = document.querySelector('.scrollindicator.scrollindicator--left');
+  var menuBar = document.querySelector('.docs-navigation');
+  var delta = 40;
+
+  function updateScrollIndicator() {
+    leftScroll.classList.remove('disabled');
+    rightScroll.classList.remove('disabled');
+    if (menuBar.scrollLeft <= 0) {
+      leftScroll.classList.add('disabled');
+    }
+    // 5px tolerance because browsers!
+    if (menuBar.scrollLeft + menuBar.clientWidth + 5 >= menuBar.scrollWidth) {
+      rightScroll.classList.add('disabled');
+    }
+  }
+  menuBar.addEventListener('scroll', updateScrollIndicator);
+  updateScrollIndicator();
+
+  function scrollMenuBar(delta) {
+    menuBar.scrollLeft += delta;
+  }
+
+  rightScroll.addEventListener('click', scrollMenuBar.bind(null, delta));
+  rightScroll.addEventListener('tap', scrollMenuBar.bind(null, delta));
+  leftScroll.addEventListener('click', scrollMenuBar.bind(null, -delta));
+  leftScroll.addEventListener('tap', scrollMenuBar.bind(null, -delta));
+})();
+
+(function() {
+  'use strict';
+
+  var downloadButton = document.querySelector('.download-button-container');
+  if (downloadButton) {
+    downloadButton.addEventListener('click', function() {
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'click',
+        eventAction: 'mdl-download'
+      });
+    });
+  }
+
+})();
+
+// Disable empty links in demo's to prevent refresh page
+(function () {
+  'use strict';
+  var emptyLinks = document.querySelectorAll('[href=""]');
+  Array.prototype.forEach.call(emptyLinks, function(link) {
+    link.addEventListener('click', function (event){
+      event.preventDefault();
+    });
+  });
+})();
