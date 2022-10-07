c2da83b6bd66f6c8b4b2558e619221d60335ec67
rodydavis <rodydavis@users.noreply.github.com>
2022-10-07 09:14:24 +0000

    deploy: c827ed2f1d89fbab716ee6f74f4d0bf4374b6b40

--- hoisted.dcf9698b.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/Options.astro_astro_type_script_index_0_lang.af928f1a.js";const e=document.querySelectorAll(".nav-link[data-selected]");e.length>0&&e[0].scrollIntoView({behavior:"smooth",block:"center",inline:"center"});
